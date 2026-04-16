c117142f2cac8d05539740e748d2c13df7239141
Minato-07 <mdmusfiq.hasan@gmail.com>
2026-04-16 16:18:27 +0600

    updated some vite file for deployment

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { HashRouter, Routes, Route } from "react-router-dom"
 import "./index.css"
 import App from "./App"
 import AllProjects from "./pages/AllProjects"
@@ -10,13 +10,13 @@ import { ThemeProvider } from "./context/ThemeContext"
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider>
-      <BrowserRouter>
+      <HashRouter>
         <ScrollToTop />
         <Routes>
           <Route path="/" element={<App />} />
           <Route path="/projects" element={<AllProjects />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </ThemeProvider>
   </React.StrictMode>
 )
